15c55b96c41496e2915534350c48908d4b7adb45
jdelasalas12 <163302111+jdelasalas12@users.noreply.github.com>
2025-07-24 22:56:37 +0800

    updates

--- script.js
+++ script.js
@@ -3,9 +3,19 @@ let searchBtn=document.querySelector('.searchBtn');
 let closeBtn=document.querySelector('.closeBtn');
 let search_box_expand=document.querySelector('.search_box_expand');
 
+
 searchBtn.onclick=function() {
     search_box_expand.classList.add('active');
 }
 closeBtn.onclick=function(){
     search_box_expand.classList.remove('active');
 }
+
+
+const menuBtn= document.querySelector('#menuBtn')
+const menu_bottom = document.querySelector('#menu_bottom')
+
+
+menuBtn.addEventListener('click' , () => {
+    menu_bottom.classList.toggle('hide')
+    })
